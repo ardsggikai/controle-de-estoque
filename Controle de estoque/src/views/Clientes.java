6125bc13dd9639ecebb06e0ea01249281bc33db9
Allan Gomes <allan.gomes070502@gmail.com>
2022-11-19 14:31:19 -0300

    Atualizacao A Clientes

--- Controle de estoque/src/views/Clientes.java
+++ Controle de estoque/src/views/Clientes.java
@@ -5,13 +5,26 @@ import java.awt.EventQueue;
 import javax.swing.JDialog;
 import javax.swing.JScrollPane;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
+
+import models.DAO;
+import net.proteanit.sql.DbUtils;
+
 import javax.swing.JTable;
 import java.awt.Font;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Clientes extends JDialog {
 
@@ -84,6 +97,12 @@ public class Clientes extends JDialog {
 		getContentPane().add(lblClientes);
 
 		txtCliente = new JTextField();
+		txtCliente.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				pesquisarCliente();
+			}
+		});
 		txtCliente.setBounds(65, 8, 86, 20);
 		getContentPane().add(txtCliente);
 		txtCliente.setColumns(10);
@@ -213,8 +232,13 @@ public class Clientes extends JDialog {
 		txtTelefone.setColumns(10);
 
 		btnCreate = new JButton("");
+		btnCreate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				pesquisar();
+			}
+		});
 		btnCreate.setIcon(new ImageIcon(Clientes.class.getResource("/img/btnCreate.png")));
-		btnCreate.setToolTipText("Adicionar fornecedor");
+		btnCreate.setToolTipText("Adicionar Cliente");
 		btnCreate.setEnabled(false);
 		btnCreate.setContentAreaFilled(false);
 		btnCreate.setBorderPainted(false);
@@ -222,8 +246,13 @@ public class Clientes extends JDialog {
 		getContentPane().add(btnCreate);
 
 		btnUpdate = new JButton("");
+		btnUpdate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				atualizar();
+			}
+		});
 		btnUpdate.setIcon(new ImageIcon(Clientes.class.getResource("/img/btnUpdate.png")));
-		btnUpdate.setToolTipText("Atualizar fornecedor");
+		btnUpdate.setToolTipText("Atualizar Cliente");
 		btnUpdate.setEnabled(false);
 		btnUpdate.setContentAreaFilled(false);
 		btnUpdate.setBorderPainted(false);
@@ -231,8 +260,13 @@ public class Clientes extends JDialog {
 		getContentPane().add(btnUpdate);
 
 		btnDelete = new JButton("");
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				deleteByidFor();
+			}
+		});
 		btnDelete.setIcon(new ImageIcon(Clientes.class.getResource("/img/btnDelete.png")));
-		btnDelete.setToolTipText("Remover fornecedor");
+		btnDelete.setToolTipText("Remover Cliente");
 		btnDelete.setEnabled(false);
 		btnDelete.setContentAreaFilled(false);
 		btnDelete.setBorderPainted(false);
@@ -240,6 +274,11 @@ public class Clientes extends JDialog {
 		getContentPane().add(btnDelete);
 
 		btnLimpar = new JButton("");
+		btnLimpar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				limpar();
+			}
+		});
 		btnLimpar.setIcon(new ImageIcon(Clientes.class.getResource("/img/BtnEraser.png")));
 		btnLimpar.setToolTipText("Botão Limpar");
 		btnLimpar.setFont(new Font("Arial", Font.PLAIN, 11));
@@ -251,7 +290,265 @@ public class Clientes extends JDialog {
 
 	}// Fim do Construtor
 
-	
+	/**
+	 * Conexao
+	 */
+	DAO dao = new DAO();
+
+	/**
+	 * Metodo Responsavel pela pesquisa avancada do Cliente usando filtro
+	 */
+	private void pesquisarCliente() {
+
+		String read2 = "select idFor as ID, fantasia as Cliente, fone1 as Telefone, fone2 as Whatsapp, nomeContato as Contato from clientes where fantasia like ?";
+		try {
+			Connection con = dao.conectar();
+			PreparedStatement pst = con.prepareStatement(read2);
+			pst.setString(1, txtCliente.getText() + "%"); // Atencao "%"
+			ResultSet rs = pst.executeQuery();
+			// Uso da Biblioteca rs2xml para "popular" da tabela //(popula��o)
+			table.setModel(DbUtils.resultSetToTableModel(rs));
+			con.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	private void pesquisar() {
+
+		/**
+		 * validacao
+		 */
+		if (txtID.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira o Id do Cliente");
+			txtID.requestFocus();
+		} else {
+			String read = "select * from clientes where idFor = ?";
+			try {
+				Connection con = dao.conectar();
+				PreparedStatement pst = con.prepareStatement(read);
+				pst.setString(1, txtID.getText());
+				ResultSet rs = pst.executeQuery();
+				if (rs.next()) {
+					txtID.setText(rs.getString(1));
+					txtNome.setText(rs.getString(2));
+					txtCpf.setText(rs.getString(3));
+					txtCep.setText(rs.getString(4));
+					txtEndereco.setText(rs.getString(5));
+					txtN.setText(rs.getString(6));
+					txtComplemento.setText(rs.getString(7));
+					txtBairro.setText(rs.getString(8));
+					txtCidade.setText(rs.getString(9));
+					cboUf.setSelectedItem(rs.getString(10));
+					txtTelefone.setText(rs.getString(11));
+					txtEmail.setText(rs.getString(12));
+
+					/**
+					 * Habilitar botoes alterar e excluir
+					 */
+					btnUpdate.setEnabled(true);
+					btnDelete.setEnabled(true);
+					btnLimpar.setEnabled(true);
+
+				} else {
+					JOptionPane.showMessageDialog(null, "Cliente n�o cadastrado");
+					btnCreate.setEnabled(true);
+					limpar();
+					txtID.requestFocus();
+				}
+				con.close();
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+
+	public void adicionar() {
+		/**
+		 * Validacao
+		 */
+
+		if (txtNome.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira O Nome");
+			txtNome.requestFocus();
+		} else if (txtCpf.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira O CPF");
+			txtCpf.requestFocus();
+		} else if (txtCep.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira o Cep");
+			txtCep.requestFocus();
+		} else if (txtEndereco.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira o Endereço");
+			txtEndereco.requestFocus();
+		} else if (txtN.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira o Numero");
+			txtN.requestFocus();
+		} else if (txtComplemento.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira o Complemento");
+			txtComplemento.requestFocus();
+		} else if (txtBairro.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira o Bairro");
+			txtBairro.requestFocus();
+		} else if (txtCidade.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira A Cidade");
+			txtCidade.requestFocus();
+		} else if (txtTelefone.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira o Telefone");
+			txtTelefone.requestFocus();
+		} else if (txtEmail.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira o Email");
+			txtEmail.requestFocus();
+		} else {
+		
+			// Logica Principal
+			String create = "insert into clientes (Nome, CPF, Cep, Endereco, Numero, Complemento, Bairro, Cidade, Uf, Telefone, Email) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			try {
+				// Abrir a conexao
+				Connection con = dao.conectar();
+				// Preparar a query (substitui�ao de parametros)
+				PreparedStatement pst = con.prepareStatement(create);
+
+				pst.setString(1, txtID.getText());
+				pst.setString(2, txtNome.getText());
+				pst.setString(3, txtCpf.getText());
+				pst.setString(4, txtCep.getText());
+				pst.setString(5, txtEndereco.getText());
+				pst.setString(6, txtN.getText());
+				pst.setString(7, txtComplemento.getText());
+				pst.setString(8, txtBairro.getText());
+				pst.setString(9, txtCidade.getText());
+				pst.setString(10, cboUf.getSelectedItem().toString());
+				pst.setString(11, txtTelefone.getText());
+				pst.setString(12, txtEmail.getText());
+
+				// Executar a query e atualizar as informa�oes no banco
+				int confirma = pst.executeUpdate();
+				if (confirma == 1) {
+					JOptionPane.showMessageDialog(null, "Funcionario cadastrado com sucesso!");
+					limpar();
+					// Encerrar a conexao
+					con.close();
+				}
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+
+	public void atualizar() {
+
+		// Valida�ao
+		if (txtNome.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira O Nome");
+			txtNome.requestFocus();
+		} else if (txtCpf.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira O CPF");
+			txtCpf.requestFocus();
+		} else if (txtCep.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira o Cep");
+			txtCep.requestFocus();
+		} else if (txtEndereco.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira o Endereço");
+			txtEndereco.requestFocus();
+		} else if (txtN.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira o Numero");
+			txtN.requestFocus();
+		} else if (txtComplemento.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira o Complemento");
+			txtComplemento.requestFocus();
+		} else if (txtBairro.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira o Bairro");
+			txtBairro.requestFocus();
+		} else if (txtCidade.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira A Cidade");
+			txtCidade.requestFocus();
+		} else if (txtTelefone.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira o Telefone");
+			txtTelefone.requestFocus();
+		} else if (txtEmail.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Insira o Email");
+			txtEmail.requestFocus();
+		} else {
+
+			// Logica Principal
+			String update = "update clientes set Nome = ?, CPF = ?, Cep = ?, Endereco = ?, Numero = ?, Complemento = ?, Bairro = ?, Cidade = ?, Uf = ?, Telefone = ?, Email = ?, where idFor = ?";
+
+			try {
+				// Abrir a conexao
+				Connection con = dao.conectar();
+				// Preparar a query (substitui�ao de parametros)
+				PreparedStatement pst = con.prepareStatement(update);
+				
+				pst.setString(1, txtNome.getText());
+				pst.setString(2, txtCpf.getText());
+				pst.setString(3, txtCep.getText());
+				pst.setString(4, txtEndereco.getText());
+				pst.setString(5, txtN.getText());
+				pst.setString(6, txtComplemento.getText());
+				pst.setString(7, txtBairro.getText());
+				pst.setString(8, txtCidade.getText());
+				pst.setString(9, cboUf.getSelectedItem().toString());
+				pst.setString(10, txtTelefone.getText());
+				pst.setString(11, txtEmail.getText());
+				pst.setString(12, txtID.getText());
+				// Executar a query e atualizar as informa�oes no banco
+				int confirma = pst.executeUpdate();
+				// System.out.println(confirma);
+				if (confirma == 1) {
+					JOptionPane.showMessageDialog(null, "Informaçoes do clientes Atualizados com Sucesso.");
+					limpar();
+				}
+
+				// Encerrar a conexao
+				con.close();
+			} catch (Exception e) {
+				System.out.println(e);
+				JOptionPane.showMessageDialog(null, "clientes Nao Atualizado");
+				limpar();
+			}
+		}
+	}
+
+	/**
+	 * Metodo De Deletar
+	 */
+
+	public void deleteByidFor() {
+
+		// Valida��o
+		int confirma = JOptionPane.showConfirmDialog(null, "Confirma a Exclus�o deste Cliente?", "Atençao",
+				JOptionPane.YES_NO_OPTION);
+		if (confirma == JOptionPane.YES_OPTION) {
+
+			String delete = "delete from clientes where idFor = ?";
+			try {
+				// abrir a conexao
+				Connection con = dao.conectar();
+				// preparar a query
+				PreparedStatement pst = con.prepareStatement(delete);
+				pst.setString(1, txtID.getText());
+				// executar o comando sql e confirmar a exclus�o
+				int confirmaExcluir = pst.executeUpdate();
+				if (confirmaExcluir == 1) {
+					limpar();
+					JOptionPane.showMessageDialog(null, "Cliente excluido com sucesso");
+				}
+				// Encerrar a conexao
+				con.close();
+			} catch (Exception e) {
+				System.out.println(e);
+				JOptionPane.showMessageDialog(null, "Cliente Nao Foi Excluido");
+				limpar();
+			}
+
+		}
+
+	}
+
+	/**
+	 * Metodo De Limpar Campos
+	 */
 	public void limpar() {
 		txtCliente.setText(null);
 		txtID.setText(null);
