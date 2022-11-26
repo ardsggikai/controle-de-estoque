6c6a6017edde688be1cb2c09b8287680f2ab5ca8
Allan Gomes <allan.gomes070502@gmail.com>
2022-11-25 22:34:20 -0300

    Atualizacao nos Produtos

--- Controle de estoque/src/views/Produtos.java
+++ Controle de estoque/src/views/Produtos.java
@@ -28,6 +28,7 @@ import javax.swing.border.TitledBorder;
 
 import com.toedter.calendar.JDateChooser;
 
+import Atxy2k.CustomTextField.RestrictedTextField;
 import models.DAO;
 import net.proteanit.sql.DbUtils;
 
@@ -43,7 +44,7 @@ public class Produtos extends JDialog {
 	private JTextField txtProduto;
 	private JTextField txtCusto;
 	private JTextField txtLucro;
-	private JTextField textField_4;
+	private JTextField txtFabricante;
 	private JTextField txtEstoque;
 	private JTextField txtEstoquemin;
 	private JTextField txtLocal;
@@ -84,55 +85,78 @@ public class Produtos extends JDialog {
 		setResizable(false);
 		setModal(true);
 		setBounds(100, 100, 800, 485);
+		setLocationRelativeTo(null);
 		getContentPane().setLayout(null);
-		
+
 		JDateChooser dateEntrada = new JDateChooser();
 		dateEntrada.setBounds(397, 191, 134, 20);
 		getContentPane().add(dateEntrada);
-		
+
 		JDateChooser dateValidade = new JDateChooser();
 		dateValidade.setBounds(611, 191, 134, 20);
 		getContentPane().add(dateValidade);
-		
+
 		JLabel lblCodigoBarras = new JLabel("");
 		lblCodigoBarras.setIcon(new ImageIcon(Produtos.class.getResource("/img/barcode.png")));
 		lblCodigoBarras.setFont(new Font("Arial", Font.PLAIN, 11));
 		lblCodigoBarras.setBounds(0, 24, 64, 45);
 		getContentPane().add(lblCodigoBarras);
-		
+
 		txtBarcode = new JTextField();
+		txtBarcode.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// valida��o (aceita somente os caracteres da String)
+				String caracteres = "0987654321";
+				if (!caracteres.contains(e.getKeyChar() + "")) {
+					e.consume();
+				}
+			}
+		});
 		txtBarcode.setFont(new Font("Arial", Font.PLAIN, 11));
 		txtBarcode.setBounds(73, 37, 240, 20);
 		getContentPane().add(txtBarcode);
 		txtBarcode.setColumns(10);
-		
+
 		JLabel lblCodigo = new JLabel("C\u00F3digo");
 		lblCodigo.setFont(new Font("Arial", Font.PLAIN, 11));
 		lblCodigo.setBounds(10, 96, 46, 14);
 		getContentPane().add(lblCodigo);
-		
+
 		txtCodigo = new JTextField();
+		txtCodigo.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// valida��o (aceita somente os caracteres da String)
+				String caracteres = "0987654321";
+				if (!caracteres.contains(e.getKeyChar() + "")) {
+					e.consume();
+				}
+			}
+		});
 		txtCodigo.setFont(new Font("Arial", Font.PLAIN, 11));
 		txtCodigo.setBounds(73, 93, 134, 20);
 		getContentPane().add(txtCodigo);
 		txtCodigo.setColumns(10);
-		
+
 		btnPesquisar = new JButton("Pesquisar");
 		btnPesquisar.setFont(new Font("Arial", Font.PLAIN, 11));
 		btnPesquisar.setBounds(217, 92, 96, 23);
 		getContentPane().add(btnPesquisar);
-		
+
 		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Fornecedor", TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel.setBorder(new TitledBorder(
+				new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)),
+				"Fornecedor", TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		panel.setBounds(335, 24, 410, 137);
 		getContentPane().add(panel);
 		panel.setLayout(null);
-		
+
 		lblLupaPesquisar = new JLabel("");
 		lblLupaPesquisar.setIcon(new ImageIcon(Produtos.class.getResource("/img/pesquisar.png")));
 		lblLupaPesquisar.setBounds(165, 11, 32, 32);
 		panel.add(lblLupaPesquisar);
-		
+
 		txtFornecedor = new JTextField();
 		txtFornecedor.addKeyListener(new KeyAdapter() {
 			@Override
@@ -144,162 +168,293 @@ public class Produtos extends JDialog {
 		txtFornecedor.setBounds(10, 24, 86, 20);
 		panel.add(txtFornecedor);
 		txtFornecedor.setColumns(10);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 50, 390, 76);
 		panel.add(scrollPane);
-		
+
 		table = new JTable();
 		scrollPane.setViewportView(table);
-		
+
 		txtIdFor = new JTextField();
+		txtIdFor.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// valida��o (aceita somente os caracteres da String)
+				String caracteres = "0987654321.";
+				if (!caracteres.contains(e.getKeyChar() + "")) {
+					e.consume();
+				}
+			}
+		});
 		txtIdFor.setBounds(290, 23, 86, 20);
 		panel.add(txtIdFor);
 		txtIdFor.setColumns(10);
-		
+
 		JLabel lblIdFor = new JLabel("ID");
 		lblIdFor.setHorizontalAlignment(SwingConstants.CENTER);
 		lblIdFor.setFont(new Font("Arial", Font.PLAIN, 11));
 		lblIdFor.setBounds(231, 26, 46, 14);
 		panel.add(lblIdFor);
-		
+
 		JLabel lblEntrada = new JLabel("Entrada");
 		lblEntrada.setFont(new Font("Arial", Font.PLAIN, 11));
 		lblEntrada.setBounds(341, 191, 46, 14);
 		getContentPane().add(lblEntrada);
-		
+
 		JLabel lblValidade = new JLabel("Validade");
 		lblValidade.setFont(new Font("Arial", Font.PLAIN, 11));
 		lblValidade.setBounds(553, 191, 46, 14);
 		getContentPane().add(lblValidade);
-		
+
 		btnAddProduto = new JButton("");
 		btnAddProduto.setIcon(new ImageIcon(Produtos.class.getResource("/img/boxadd.png")));
 		btnAddProduto.setFont(new Font("Arial", Font.PLAIN, 11));
 		btnAddProduto.setBounds(423, 356, 64, 64);
 		getContentPane().add(btnAddProduto);
-		
+
 		btnUpdateProduto = new JButton("");
 		btnUpdateProduto.setIcon(new ImageIcon(Produtos.class.getResource("/img/boxupdate.png")));
 		btnUpdateProduto.setFont(new Font("Arial", Font.PLAIN, 11));
 		btnUpdateProduto.setBounds(533, 356, 64, 64);
 		getContentPane().add(btnUpdateProduto);
-		
+
 		btnDeleteProduto = new JButton("");
 		btnDeleteProduto.setIcon(new ImageIcon(Produtos.class.getResource("/img/boxdel.png")));
 		btnDeleteProduto.setFont(new Font("Arial", Font.PLAIN, 11));
 		btnDeleteProduto.setBounds(643, 356, 64, 64);
 		getContentPane().add(btnDeleteProduto);
-		
+
 		txtaDescricao = new JTextArea();
+		txtaDescricao.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// valida��o (aceita somente os caracteres da String)
+				String caracteres = "AaBbcCdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+				if (!caracteres.contains(e.getKeyChar() + "")) {
+					e.consume();
+				}
+			}
+		});
 		txtaDescricao.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		txtaDescricao.setBounds(73, 215, 242, 64);
 		getContentPane().add(txtaDescricao);
-		
+
 		JLabel lblDescricao = new JLabel("Descri\u00E7\u00E3o");
 		lblDescricao.setFont(new Font("Arial", Font.PLAIN, 11));
 		lblDescricao.setBounds(10, 220, 64, 14);
 		getContentPane().add(lblDescricao);
-		
+
 		JLabel lblProduto = new JLabel("Produto");
 		lblProduto.setFont(new Font("Arial", Font.PLAIN, 11));
 		lblProduto.setBounds(10, 159, 46, 14);
 		getContentPane().add(lblProduto);
-		
+
 		txtProduto = new JTextField();
+		txtProduto.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// valida��o (aceita somente os caracteres da String)
+				String caracteres = "AaBbcCdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+				if (!caracteres.contains(e.getKeyChar() + "")) {
+					e.consume();
+				}
+			}
+		});
 		txtProduto.setFont(new Font("Arial", Font.PLAIN, 11));
 		txtProduto.setBounds(73, 156, 240, 20);
 		getContentPane().add(txtProduto);
 		txtProduto.setColumns(10);
-		
+
 		JLabel lblCusto = new JLabel("Custo");
 		lblCusto.setFont(new Font("Arial", Font.PLAIN, 11));
 		lblCusto.setBounds(341, 234, 46, 14);
 		getContentPane().add(lblCusto);
-		
+
 		txtCusto = new JTextField();
+		txtCusto.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// valida��o (aceita somente os caracteres da String)
+				String caracteres = "0987654321.";
+				if (!caracteres.contains(e.getKeyChar() + "")) {
+					e.consume();
+				}
+			}
+		});
 		txtCusto.setFont(new Font("Arial", Font.PLAIN, 11));
 		txtCusto.setBounds(397, 231, 114, 20);
 		getContentPane().add(txtCusto);
 		txtCusto.setColumns(10);
-		
+
 		JLabel lblLucro = new JLabel("Lucro");
 		lblLucro.setFont(new Font("Arial", Font.PLAIN, 11));
 		lblLucro.setBounds(553, 234, 46, 14);
 		getContentPane().add(lblLucro);
-		
+
 		txtLucro = new JTextField();
+		txtLucro.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// valida��o (aceita somente os caracteres da String)
+				String caracteres = "0987654321";
+				if (!caracteres.contains(e.getKeyChar() + "")) {
+					e.consume();
+				}
+			}
+		});
 		txtLucro.setFont(new Font("Arial", Font.PLAIN, 11));
 		txtLucro.setBounds(611, 231, 114, 20);
 		getContentPane().add(txtLucro);
 		txtLucro.setColumns(10);
-		
+
 		JLabel lblPorcentagem = new JLabel("%");
 		lblPorcentagem.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPorcentagem.setFont(new Font("Arial", Font.PLAIN, 15));
 		lblPorcentagem.setBounds(716, 233, 46, 14);
 		getContentPane().add(lblPorcentagem);
-		
+
 		JLabel lblFabricante = new JLabel("Fabricante");
 		lblFabricante.setFont(new Font("Arial", Font.PLAIN, 11));
 		lblFabricante.setBounds(10, 311, 64, 14);
 		getContentPane().add(lblFabricante);
-		
-		textField_4 = new JTextField();
-		textField_4.setFont(new Font("Arial", Font.PLAIN, 11));
-		textField_4.setBounds(73, 308, 242, 20);
-		getContentPane().add(textField_4);
-		textField_4.setColumns(10);
-		
+
+		txtFabricante = new JTextField();
+		txtFabricante.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// valida��o (aceita somente os caracteres da String)
+				String caracteres = "AaBbcCdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+				if (!caracteres.contains(e.getKeyChar() + "")) {
+					e.consume();
+				}
+			}
+		});
+		txtFabricante.setFont(new Font("Arial", Font.PLAIN, 11));
+		txtFabricante.setBounds(73, 308, 242, 20);
+		getContentPane().add(txtFabricante);
+		txtFabricante.setColumns(10);
+
 		JLabel lblEstoque = new JLabel("Estoque");
 		lblEstoque.setFont(new Font("Arial", Font.PLAIN, 11));
 		lblEstoque.setBounds(10, 361, 46, 14);
 		getContentPane().add(lblEstoque);
-		
+
 		txtEstoque = new JTextField();
+		txtEstoque.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// valida��o (aceita somente os caracteres da String)
+				String caracteres = "0987654321";
+				if (!caracteres.contains(e.getKeyChar() + "")) {
+					e.consume();
+				}
+			}
+		});
 		txtEstoque.setFont(new Font("Arial", Font.PLAIN, 11));
 		txtEstoque.setBounds(73, 358, 75, 20);
 		getContentPane().add(txtEstoque);
 		txtEstoque.setColumns(10);
-		
+
 		txtEstoquemin = new JTextField();
+		txtEstoquemin.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// valida��o (aceita somente os caracteres da String)
+				String caracteres = "0987654321";
+				if (!caracteres.contains(e.getKeyChar() + "")) {
+					e.consume();
+				}
+			}
+		});
 		txtEstoquemin.setFont(new Font("Arial", Font.PLAIN, 11));
 		txtEstoquemin.setColumns(10);
 		txtEstoquemin.setBounds(240, 356, 75, 20);
 		getContentPane().add(txtEstoquemin);
-		
+
 		JLabel lblEstoquemin = new JLabel("Estoque Minimo");
 		lblEstoquemin.setFont(new Font("Arial", Font.PLAIN, 11));
 		lblEstoquemin.setBounds(158, 361, 84, 14);
 		getContentPane().add(lblEstoquemin);
-		
+
 		JLabel lblUnidade = new JLabel("Unidade");
 		lblUnidade.setFont(new Font("Arial", Font.PLAIN, 11));
 		lblUnidade.setBounds(10, 406, 46, 14);
 		getContentPane().add(lblUnidade);
-		
+
 		cboUnidade = new JComboBox<Object>();
-		cboUnidade.setModel(new DefaultComboBoxModel<Object>(new String[] {"", "(k)\t", "(h)\t", "(da)\t\t ", "(d)\t ", "(c)\t", "(m)", "(kl)\t", "(hl)\t", "(dal)\t", "(l)\t", "(dl)", "(cl)\t", "(ml)", "(km)\t", "(hm)\t", "(dam)\t", "(m)\t", "(dm)\t", "(cm)\t", "(ml)", "(kg)\t", "(hg)\t", "(dag)\t", "(g)\t", "(dg)\t", "(cg)\t", "(mg)", "(km3)\t", "hm3)\t", "(dam3)\t", "(m3)\t", "(dm3)\t", "(cm3)\t", "(mm3)"}));
+		cboUnidade.setModel(new DefaultComboBoxModel<Object>(new String[] { "", "(k)\t", "(h)\t", "(da)\t\t ", "(d)\t ",
+				"(c)\t", "(m)", "(kl)\t", "(hl)\t", "(dal)\t", "(l)\t", "(dl)", "(cl)\t", "(ml)", "(km)\t", "(hm)\t",
+				"(dam)\t", "(m)\t", "(dm)\t", "(cm)\t", "(ml)", "(kg)\t", "(hg)\t", "(dag)\t", "(g)\t", "(dg)\t",
+				"(cg)\t", "(mg)", "(km3)\t", "hm3)\t", "(dam3)\t", "(m3)\t", "(dm3)\t", "(cm3)\t", "(mm3)" }));
 		cboUnidade.setFont(new Font("Arial", Font.PLAIN, 11));
 		cboUnidade.setBounds(73, 398, 64, 22);
 		getContentPane().add(cboUnidade);
-		
+
 		JLabel lblLocal = new JLabel("Local");
 		lblLocal.setFont(new Font("Arial", Font.PLAIN, 11));
 		lblLocal.setBounds(158, 406, 39, 14);
 		getContentPane().add(lblLocal);
-		
+
 		txtLocal = new JTextField();
+		txtLocal.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// valida��o (aceita somente os caracteres da String)
+				String caracteres = "AaBbcCdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+				if (!caracteres.contains(e.getKeyChar() + "")) {
+					e.consume();
+				}
+			}
+		});
 		txtLocal.setFont(new Font("Arial", Font.PLAIN, 11));
 		txtLocal.setBounds(205, 400, 108, 20);
 		getContentPane().add(txtLocal);
 		txtLocal.setColumns(10);
 
+		// Uso da biblioteca atxy2k para restringir o maximo de caracteres
+		// txtBarcode
+		RestrictedTextField Barcode = new RestrictedTextField(txtBarcode);
+		Barcode.setLimit(255);
+		// txtCodigo
+		RestrictedTextField Codigo = new RestrictedTextField(txtCodigo);
+		Codigo.setLimit(30);
+		// txtProduto
+		RestrictedTextField Produto = new RestrictedTextField(txtProduto);
+		Produto.setLimit(55);
+		// txtFabricante
+		RestrictedTextField Fabricante = new RestrictedTextField(txtFabricante);
+		Fabricante.setLimit(50);
+		// txtEstoque
+		RestrictedTextField Estoque = new RestrictedTextField(txtEstoque);
+		Estoque.setLimit(1000);
+		// txtEstoque
+		RestrictedTextField EstoqueMin = new RestrictedTextField(txtEstoquemin);
+		EstoqueMin.setLimit(1000);
+		// txtLocal
+		RestrictedTextField Local = new RestrictedTextField(txtLocal);
+		Local.setLimit(50);
+		// txtFornecedor
+		RestrictedTextField Fornecedor = new RestrictedTextField(txtFornecedor);
+		Fornecedor.setLimit(50);
+
+		// txtIdFor
+		RestrictedTextField IdFor = new RestrictedTextField(txtIdFor);
+		IdFor.setLimit(8);
+
+		// txtCusto
+		RestrictedTextField Custo = new RestrictedTextField(txtCusto);
+		Custo.setLimit(99);
+
+		// txtLucro
+		RestrictedTextField Lucro = new RestrictedTextField(txtLucro);
+		Lucro.setLimit(99);
+
 	}// Fim Construtor
-	
+
 	DAO dao = new DAO();
 	private JTable table;
-	
+
 	/**
 	 * Metodo Responsavel pela pesquisa avancada do fornecedor usando filtro
 	 */
